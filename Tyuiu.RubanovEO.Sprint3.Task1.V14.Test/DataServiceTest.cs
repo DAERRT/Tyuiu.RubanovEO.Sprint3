fe1e5c33c7f10348ffd7a8c0d37dbca53484b00d
ЧЕЛОВЕК ЧЕЛОВЕК <olegoviz.2006@gmail.com>
2024-10-02 11:20:34 +0500

    Task1

---
+++ Tyuiu.RubanovEO.Sprint3.Task1.V14.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.RubanovEO.Sprint3.Task1.V14.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Tyuiu.RubanovEO.Sprint3.Task1.V14/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Tyuiu.RubanovEO.Sprint3.Task1.V14.Lib;
+
+namespace Tyuiu.RubanovEO.Sprint3.Task1.V14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine(ds.GetSumSeries(0.7,1,14));
+    }
+}
